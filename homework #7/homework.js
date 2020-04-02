9e83cd516a40f914d9167a15406c0ee0dfb41045
Kommissare <61708092+Kommissare@users.noreply.github.com>
2020-04-02 16:03:17 +0300

    HomeWork#7

---
+++ homework #7/homework.js
@@ -0,0 +1,23 @@
+let getPosts = () => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(response => response.json())
+    .then((json) => {
+        console.log(json);
+        return json;
+    })
+    .then((data) => {
+        createPost(data);
+    });
+}
+
+let createPost = (data) => {
+    let list = document.getElementById('list');
+    for(let i = 0; i < 12; i++) {
+        let div = document.createElement('div')
+        div.innerText = data[i].title;
+        div.classList.add('post');
+        list.append(div)
+    }
+}
+
+document.querySelector('.btn').addEventListener('click', getPosts);
